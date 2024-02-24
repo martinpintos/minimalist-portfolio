0b0b05f19753acf6b853e2272ad8ff0bdefb8b0b
Martín Pintos <martin.d.pintos@gmail.com>
2024-02-24 02:00:05 -0300

    feat(skills): add skills list

--- components/sections/skills.tsx
+++ components/sections/skills.tsx
@@ -1,5 +1,19 @@
 import Section from "@/components/section";
+import cv from "@/lib/cv.json";
+import { Chip } from "@nextui-org/react";
 
 export default function Skills() {
-  return <Section title="Habilidades">Skills</Section>;
+  return (
+    <Section title="Habilidades">
+      <ul className="flex flex-wrap">
+        {cv.skills.map(({ name }) => (
+          <li key={name} className="mr-2">
+            <Chip size="sm" color="success">
+              {name}
+            </Chip>
+          </li>
+        ))}
+      </ul>
+    </Section>
+  );
 }
